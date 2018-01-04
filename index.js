a5de68c0444bd11a817f33fc1524ca19957a5faf
MarkyMark <marky@MarkyMarks-MacBook-Pro.local>
2018-01-04 18:48:47 +0100

    koa dependencies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//Koa Server
 const Koa = require('koa');
 const app = new Koa();
+//Middleware
+const bodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+//File imports
+const Routes = require("./routes/router.js");
 
-
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
 app.listen(3000, console.log('running hard on 3000'));
