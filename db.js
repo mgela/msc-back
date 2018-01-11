c533a891cf046b78b863756ce5502f4e8923bf28
MarkyMark <mgelabert3@gmail.com>
2018-01-11 20:56:30 +0100

    heroku and mlab deploy

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/MSC', {useMongoClient: true});
+mongoose.connect('mongodb://admin:password@ds251827.mlab.com:51827/mahskicoach', {useMongoClient: true});
 module.exports = mongoose;
